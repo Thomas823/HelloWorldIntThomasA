be697d455e1c279e4714170dd26a3f5216f13c11
Thomas823 <35612259+Thomas823@users.noreply.github.com>
2018-04-05 16:06:05 -0400

    Final

--- HelloWorldIntThomasA/HelloWorldIntForm.Designer.cs
+++ HelloWorldIntThomasA/HelloWorldIntForm.Designer.cs
@@ -30,10 +30,19 @@
         {
             this.grbLanguageOptions = new System.Windows.Forms.GroupBox();
             this.lblGreeting = new System.Windows.Forms.Label();
+            this.radEnglish = new System.Windows.Forms.RadioButton();
+            this.radFrench = new System.Windows.Forms.RadioButton();
+            this.radChinese = new System.Windows.Forms.RadioButton();
+            this.radArabic = new System.Windows.Forms.RadioButton();
+            this.grbLanguageOptions.SuspendLayout();
             this.SuspendLayout();
             // 
             // grbLanguageOptions
             // 
+            this.grbLanguageOptions.Controls.Add(this.radArabic);
+            this.grbLanguageOptions.Controls.Add(this.radChinese);
+            this.grbLanguageOptions.Controls.Add(this.radFrench);
+            this.grbLanguageOptions.Controls.Add(this.radEnglish);
             this.grbLanguageOptions.Location = new System.Drawing.Point(50, 137);
             this.grbLanguageOptions.Name = "grbLanguageOptions";
             this.grbLanguageOptions.Size = new System.Drawing.Size(397, 100);
@@ -51,6 +60,54 @@
             this.lblGreeting.TabIndex = 0;
             this.lblGreeting.Text = "Hello, World!";
             // 
+            // radEnglish
+            // 
+            this.radEnglish.AutoSize = true;
+            this.radEnglish.Location = new System.Drawing.Point(28, 42);
+            this.radEnglish.Name = "radEnglish";
+            this.radEnglish.Size = new System.Drawing.Size(59, 17);
+            this.radEnglish.TabIndex = 0;
+            this.radEnglish.TabStop = true;
+            this.radEnglish.Text = "English";
+            this.radEnglish.UseVisualStyleBackColor = true;
+            this.radEnglish.CheckedChanged += new System.EventHandler(this.radEnglish_CheckedChanged);
+            // 
+            // radFrench
+            // 
+            this.radFrench.AutoSize = true;
+            this.radFrench.Location = new System.Drawing.Point(124, 42);
+            this.radFrench.Name = "radFrench";
+            this.radFrench.Size = new System.Drawing.Size(58, 17);
+            this.radFrench.TabIndex = 1;
+            this.radFrench.TabStop = true;
+            this.radFrench.Text = "French";
+            this.radFrench.UseVisualStyleBackColor = true;
+            this.radFrench.CheckedChanged += new System.EventHandler(this.radFrench_CheckedChanged);
+            // 
+            // radChinese
+            // 
+            this.radChinese.AutoSize = true;
+            this.radChinese.Location = new System.Drawing.Point(215, 42);
+            this.radChinese.Name = "radChinese";
+            this.radChinese.Size = new System.Drawing.Size(63, 17);
+            this.radChinese.TabIndex = 2;
+            this.radChinese.TabStop = true;
+            this.radChinese.Text = "Chinese";
+            this.radChinese.UseVisualStyleBackColor = true;
+            this.radChinese.CheckedChanged += new System.EventHandler(this.radChinese_CheckedChanged);
+            // 
+            // radArabic
+            // 
+            this.radArabic.AutoSize = true;
+            this.radArabic.Location = new System.Drawing.Point(306, 42);
+            this.radArabic.Name = "radArabic";
+            this.radArabic.Size = new System.Drawing.Size(55, 17);
+            this.radArabic.TabIndex = 3;
+            this.radArabic.TabStop = true;
+            this.radArabic.Text = "Arabic";
+            this.radArabic.UseVisualStyleBackColor = true;
+            this.radArabic.CheckedChanged += new System.EventHandler(this.radArabic_CheckedChanged);
+            // 
             // frmHelloWorldInt
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -61,6 +118,8 @@
             this.Controls.Add(this.grbLanguageOptions);
             this.Name = "frmHelloWorldInt";
             this.Text = "Hello World International By Thomas Aubin";
+            this.grbLanguageOptions.ResumeLayout(false);
+            this.grbLanguageOptions.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +129,10 @@
 
         private System.Windows.Forms.GroupBox grbLanguageOptions;
         private System.Windows.Forms.Label lblGreeting;
+        private System.Windows.Forms.RadioButton radArabic;
+        private System.Windows.Forms.RadioButton radChinese;
+        private System.Windows.Forms.RadioButton radFrench;
+        private System.Windows.Forms.RadioButton radEnglish;
     }
 }
 
--- HelloWorldIntThomasA/HelloWorldIntForm.cs
+++ HelloWorldIntThomasA/HelloWorldIntForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Thomas Aubin
+ * Created on: 5 April, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #5 - Hello Wolrd International
+ * This program display's hello world in various languages
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +23,30 @@ namespace HelloWorldIntThomasA
         public frmHelloWorldInt()
         {
             InitializeComponent();
+        }
+
+        // Display hello world in english
+        private void radEnglish_CheckedChanged(object sender, EventArgs e)
+        {
+            lblGreeting.Text = "Hello World!";
+        }
+
+        // Display hello world in french
+        private void radFrench_CheckedChanged(object sender, EventArgs e)
+        {
+            lblGreeting.Text = "Bonjour Le Monde!";
+        }
+
+        // Display hello world in chinese
+        private void radChinese_CheckedChanged(object sender, EventArgs e)
+        {
+            lblGreeting.Text = "你好，世界!";
+        }
+
+        // Display hello world in arabic
+        private void radArabic_CheckedChanged(object sender, EventArgs e)
+        {
+            lblGreeting.Text = "مرحبا بالعالم!";
         }
     }
 }
